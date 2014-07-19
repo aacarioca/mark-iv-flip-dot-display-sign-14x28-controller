8b52f5c3967ceb30b374f6ff7bc6d0371da25910
aacarioca <guto.carioca@gmail.com>
2014-07-19 18:59:42 +1000

    Further format changes

--- firmware/mark-iv-flip-dot-display-sign-14x28-controller.cpp
+++ firmware/mark-iv-flip-dot-display-sign-14x28-controller.cpp
@@ -130,7 +130,7 @@ void DotDisplay::updateDisplay(char textMessage[], char log[]){
 	int currentRow = 0; 
 	
 	char temp[500];
-	sprintf(log, "_maxRowLength:%i, _maxNumRows:%i, MESSAGE:%s ", _maxRowLength, _maxNumRows,textMessage);
+	sprintf(log, "_maxRowLength:%i, _maxNumRows:%i, MESSAGE:%s\n", _maxRowLength, _maxNumRows,textMessage);
 	
 	//goes through all characters
 	for (int ch = 0; ch < (_maxMessageLength);ch++){  
@@ -140,7 +140,7 @@ void DotDisplay::updateDisplay(char textMessage[], char log[]){
 		//Serial.println(alphabetIndex);
 		//strcat(log,"Index: ");
 		//strcat(log,alphabetIndex);
-		sprintf(temp, "Character:%c ", textMessage[ch]);
+		sprintf(temp, "Character[%i]:%c ",ch, textMessage[ch]);
 		
 		if ((alphabetIndex < 0) or (ch >=strlen(textMessage))) alphabetIndex=0; 
 		
